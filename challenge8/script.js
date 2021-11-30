a2b0df505122abb199c6841d180c8fef931e997c
Stebou <stebou8mendy@gmail.com>
2021-11-30 17:52:31 +0100

    Update script.js

--- challenge8/script.js
+++ challenge8/script.js
@@ -1,5 +1,7 @@
 "use strict";
+
 const notes = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
 const calcTip = (note) => {
   const tip = note >= 50 && 350 <= note ? 0.15 : 0.2;
   return note * tip;
@@ -9,19 +11,20 @@ const calcAverage = (myArray) => {
   const reducer = (previousValue, currentValue) => previousValue + currentValue;
   const sum = myArray.reduce(reducer);
   const result = sum / myArray.length;
-  return parseFloat(result).toFixed(2);
+  return result.toFixed(2);
 };
 
 const tips = [];
 const totals = [];
 
-notes.forEach((note) => {
+notes.map((note) => {
   let tip = calcTip(note);
   tips.push(tip);
   totals.push(note + tip);
 });
 
-console.log("tips", tips, "totals", totals);
+// console.log("tips", tips, "totals", totals);
+
 console.log(
   `Average tips: ${calcAverage(tips)} ------  Average totals: ${calcAverage(
     totals
