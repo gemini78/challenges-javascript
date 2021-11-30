63286948b276f744ee491b62ac3609206fed665a
Stebou <stebou8mendy@gmail.com>
2021-11-30 17:45:24 +0100

    Update script.js

--- challenge7/script.js
+++ challenge7/script.js
@@ -1,19 +1,24 @@
 "use strict";
 
 const bills = [125, 555, 44];
+
 const calcTip = (note) => {
   const tip = note >= 50 && 350 <= note ? 0.15 : 0.2;
   return note * tip;
 };
 
 const tips = [];
-bills.forEach((note) => {
+
+bills.map(note=>{
   tips.push(calcTip(note));
 });
-// console.log(tips);
+
+//console.log(tips);
 
 const totals = [];
-for (let index = 0; index < bills.length; index++) {
-  totals[index] = bills[index] + tips[index];
-}
+
+bills.map((bill, index) => {
+  totals[index] = bill + tips[index];
+}); 
+
 console.log("totals", totals);
